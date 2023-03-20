b3d5c138f03f727d3022daff634273a899dbaba9
Omar_chel <102540602+Omar0724@users.noreply.github.com>
2023-03-20 22:34:29 +0000

    models

---
+++ Aso-Back/database/migrations/2023_03_20_094642_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('imgSrc');
+            $table->string('destTitle');
+            $table->string('location');
+            $table->string('grade');
+            $table->string('fees');
+            $table->string('description');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
